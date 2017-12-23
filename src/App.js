05d90860378b168c71052fd08de25947fadcc573
Jayanth <jayanth_mce@yahoo.com>
2017-12-23 08:51:39 -0800

    changes to header component
    changes to header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import MainSection from './components/MainSection'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header></Header>
         <MainSection></MainSection>
       </div>
     );
--- src/components/TodoTextInput.js
+++ src/components/TodoTextInput.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 export default class TodoTextInput extends Component {
+    
+    handleSave = text => {
+        if(text.length != 0) {
+            this.props.addTodo(text);
+        }
+    }
+    
     render() {
         return (
             <input 
                 type="text"
-                placeholder={this.props.placeholder} />
+                placeholder={this.props.placeholder}
+                onSave={this.handleSave} />
         );
     }
 }
